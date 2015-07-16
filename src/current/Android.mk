7a4ae74b82f084e63b6db533778db3e045546e0c
Kazuki Terada <kazuki.terada@sonymobile.com>
2015-07-16 22:05:57 +0900

    Change Android.mk to generate a static library
    This change is intended to make it possible to use "iozone" as
    a static library in another application such as unit test
    executables, and so on.
    
    If you'd like to use "iozone" from your application, you can
    just call the function, IOZone_main(int argc, char **argv).

--- src/current/Android.mk
+++ src/current/Android.mk
@@ -2,17 +2,36 @@ ifneq ($(TARGET_SIMULATOR),true)
 
 LOCAL_PATH := $(call my-dir)
 
-# Build iozone
+# Build iozone as executable
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
 	libbif.c iozone.c
 
-LOCAL_CFLAGS := -DLINUX_ARM -Dlinux -DHAVE_ANSIC_C -DO_RSYNC=O_SYNC -DNAME='"linux-android"'
+LOCAL_CFLAGS := \
+	-DLINUX_ARM -Dlinux -DHAVE_ANSIC_C -DO_RSYNC=O_SYNC \
+	-DNAME='"linux-android"' -D__off64_t_defined
 LOCAL_MODULE := iozone
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng tests
+LOCAL_MULTILIB := 64
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 
 include $(BUILD_EXECUTABLE)
+
+# Build iozone as static library
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	libbif.c iozone.c
+
+LOCAL_CFLAGS := \
+	-DLINUX_ARM -Dlinux -DHAVE_ANSIC_C -DO_RSYNC=O_SYNC \
+	-DNAME='"linux-android"' -D__off64_t_defined \
+	-Dmain=IOZone_main
+LOCAL_MODULE := libiozone
+LOCAL_MODULE_TAGS := eng tests
+LOCAL_MULTILIB := 64
+
+include $(BUILD_STATIC_LIBRARY)
 
 endif # ! TARGET_SIMULATOR
